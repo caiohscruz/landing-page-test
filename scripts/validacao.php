9dc5007636583d763e065e125f9f3b957cca830c
caiohscruz <caiohscruz@hotmail.com>
2021-09-28 10:16:29 -0300

    fix eregi deprecated

--- scripts/validacao.php
+++ scripts/validacao.php
@@ -1,14 +1,8 @@
 <?php
-// Campo que fez requisição
 $campo = $_GET['campo'];
-// Valor do campo que fez requisição
 $valor = $_GET['valor'];
-
-// Verificando o valor do campo email
-if (!eregi("^[a-z0-9_\.\-]+@[a-z0-9_\.\-]*[a-z0-9_\-]+\.[a-z]{2,4}$", $valor)) {
-    echo "Preencha com um email válido"; //
-}
-
-// Acentuação
-header("Content-Type: text/html; charset=ISO-8859-1",true);
+	
+	if (!preg_match("/^[a-z0-9_\.\-]+@[a-z0-9_\.\-]*[a-z0-9_\-]+\.[a-z]{2,4}$/i", $valor)) {
+		echo "Preencha com um email válido";
+	}
 ?>
